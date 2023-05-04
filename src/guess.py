df39bb241256b178474e5020876fa6f058e3917a
wenxuan-pan <panwenxuan42@gmail.com>
2023-05-05 09:29:36 +1000

    splitting up check input functions

--- src/guess.py
+++ src/guess.py
@@ -10,30 +10,33 @@ def random_word(words):
     return random.choice(words)
 
 
-def check_guess(answer, guess):
-    if guess == answer:
-        result = 'correct answer'
-    elif guess.isalpha() and len(guess) == 5:
-        # check each letter
-        result = [0, 0, 0, 0, 0]
-        answer_list = list(answer)
-        # check for correct letters; compared letters will be removed in the answer list to avoid duplicates
-        for index, letter in enumerate(guess):
-            if letter == answer_list[index]:
-                result[index] = 2
-                answer_list[index] = 0
-        # check for misplaced letters
-        for index, letter in enumerate(guess):
-            if letter in answer_list:
-                result[index] = 1
-                answer_list[answer_list.index(letter)] = 0
-    else:
-        result = 'input not valid'
+# check the user input is a valid 5 letter word
+def validate_input(guess):
+    if guess.isalpha() and len(guess) == 5:
+        return True
+    return False
+
+
+# check if the input is correct
+def check_exact_match(answer, guess):
+    if guess.lower() == answer.lower():
+        return True
+    return False
+
+
+# analyse user input and compare each letter
+def check_letter(answer, guess):
+    # check each letter
+    result = [0, 0, 0, 0, 0]
+    answer_list = list(answer)
+    # check for correct letters; compared letters will be removed in the answer list to avoid duplicates
+    for index, letter in enumerate(guess):
+        if letter == answer_list[index]:
+            result[index] = 2
+            answer_list[index] = 0
+    # check for misplaced letters
+    for index, letter in enumerate(guess):
+        if letter in answer_list:
+            result[index] = 1
+            answer_list[answer_list.index(letter)] = 0
     return result
-
-# answer = random_word(word_list)
-# print(f'for dev: the answer is {answer}')
-# guess = input("take a guess! ")
-
-
-print(check_guess('party', 'apple'))
--- src/test_guess.py
+++ src/test_guess.py
@@ -8,25 +8,29 @@ def test_random_word():
     assert word in guess.word_list
 
 
-def test_check_guess_correct():
-    assert guess.check_guess('apple', 'apple') == 'correct answer'
+def test_validate_input():
+    assert guess.validate_input('appl') == False
+    assert guess.validate_input('apple') == True
 
 
-def test_check_guess_invalid():
-    assert guess.check_guess('apple', 'appl') == 'input not valid'
+def test_check_exact_match():
+    assert guess.check_exact_match('apple', 'APPLE') == True
+    assert guess.check_exact_match('apple', 'Apple') == True
+    assert guess.check_exact_match('apple', 'apple') == True
+    assert guess.check_exact_match('apple', 'purple') == False
 
 
-def test_check_guess_continue():
-    assert guess.check_guess(
+def test_check_letter_normal():
+    assert guess.check_letter(
         'party', 'apple') == [1, 1, 0, 0, 0]
-    assert guess.check_guess(
+    assert guess.check_letter(
         'apple', 'party') == [1, 1, 0, 0, 0]
 
 
-def test_check_guess_duplicate():
-    assert guess.check_guess(
+def test_check_letter_duplicate():
+    assert guess.check_letter(
         'eaten', 'lever') == [0, 1, 0, 2, 0]
-    assert guess.check_guess(
+    assert guess.check_letter(
         'apple', 'puple') == [1, 0, 2, 2, 2]
-    assert guess.check_guess(
+    assert guess.check_letter(
         'apple', 'puppy') == [1, 0, 2, 0, 0]
