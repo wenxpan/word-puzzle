ce8486c47f8fa0167a2459e43bde674e13df753b
wenxuan-pan <panwenxuan42@gmail.com>
2023-05-04 21:02:58 +1000

    added check guess function; to refactor later

--- src/guess.py
+++ src/guess.py
@@ -10,4 +10,30 @@ def random_word(words):
     return random.choice(words)
 
 
-random_word(word_list)
+def check_guess(answer, guess):
+    if guess == answer:
+        result = 'correct answer'
+    elif guess.isalpha() and len(guess) == 5:
+        # check each letter
+        result = [0, 0, 0, 0, 0]
+        answer_list = list(answer)
+        # check for correct letters; compared letters will be removed in the answer list to avoid duplicates
+        for index, letter in enumerate(guess):
+            if letter == answer_list[index]:
+                result[index] = 2
+                answer_list[index] = 0
+        # check for misplaced letters
+        for index, letter in enumerate(guess):
+            if letter in answer_list:
+                result[index] = 1
+                answer_list[answer_list.index(letter)] = 0
+    else:
+        result = 'input not valid'
+    return result
+
+# answer = random_word(word_list)
+# print(f'for dev: the answer is {answer}')
+# guess = input("take a guess! ")
+
+
+print(check_guess('party', 'apple'))
--- src/test_guess.py
+++ src/test_guess.py
@@ -6,3 +6,27 @@ def test_random_word():
     assert len(word) == 5
     assert word.isalpha()
     assert word in guess.word_list
+
+
+def test_check_guess_correct():
+    assert guess.check_guess('apple', 'apple') == 'correct answer'
+
+
+def test_check_guess_invalid():
+    assert guess.check_guess('apple', 'appl') == 'input not valid'
+
+
+def test_check_guess_continue():
+    assert guess.check_guess(
+        'party', 'apple') == [1, 1, 0, 0, 0]
+    assert guess.check_guess(
+        'apple', 'party') == [1, 1, 0, 0, 0]
+
+
+def test_check_guess_duplicate():
+    assert guess.check_guess(
+        'eaten', 'lever') == [0, 1, 0, 2, 0]
+    assert guess.check_guess(
+        'apple', 'puple') == [1, 0, 2, 2, 2]
+    assert guess.check_guess(
+        'apple', 'puppy') == [1, 0, 2, 0, 0]
