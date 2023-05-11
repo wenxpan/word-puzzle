0e6cd671bef32990927d4643c5c6622b4e9e366e
wenxuan-pan <panwenxuan42@gmail.com>
2023-05-11 14:52:05 +1000

    updated test files

--- src/test_guess.py
+++ src/test_guess.py
@@ -3,20 +3,19 @@ import guess
 
 def test_random_word():
     word = guess.get_random_word(guess.word_list)
-    assert len(word) == 5
     assert word.isalpha()
     assert word in guess.word_list
 
 
 def test_check_input_word_correct(monkeypatch, capsys):
     monkeypatch.setattr('builtins.input', lambda _: 'apple')
-    assert guess.check_input_word([]) == 'APPLE'
+    assert guess.check_input_word([], 5) == 'APPLE'
 
 
 def test_check_input_word_invalid(monkeypatch, capsys):
     inputs = iter(['33', 'apple'])
     monkeypatch.setattr('builtins.input', lambda _: next(inputs))
-    result = guess.check_input_word([])
+    result = guess.check_input_word([], 5)
     captured = capsys.readouterr()
     assert captured.out == 'Input not valid. Please enter a 5-letter English word\n\n'
     assert result == 'APPLE'
@@ -25,7 +24,7 @@ def test_check_input_word_invalid(monkeypatch, capsys):
 def test_check_input_word_guessed(monkeypatch, capsys):
     inputs = iter(['apple', 'pupil'])
     monkeypatch.setattr('builtins.input', lambda _: next(inputs))
-    result = guess.check_input_word(['APPLE'])
+    result = guess.check_input_word(['APPLE'], 5)
     captured = capsys.readouterr()
     assert captured.out == 'You already guessed this word!\n\n'
     assert result == 'PUPIL'
@@ -34,7 +33,7 @@ def test_check_input_word_guessed(monkeypatch, capsys):
 def test_check_input_word_incorrect(monkeypatch, capsys):
     inputs = iter(['appli', 'apply'])
     monkeypatch.setattr('builtins.input', lambda _: next(inputs))
-    result = guess.check_input_word([])
+    result = guess.check_input_word([], 5)
     captured = capsys.readouterr()
     assert captured.out == 'Word not found in dictionary.\n\n'
     assert result == 'APPLY'
@@ -49,20 +48,20 @@ def test_check_exact_match():
 
 def test_check_letter_normal():
     assert guess.check_letter(
-        'party', 'apple') == [1, 1, 0, 0, 0]
+        'party', 'apple', 5) == [1, 1, 0, 0, 0]
     assert guess.check_letter(
-        'apple', 'party') == [1, 1, 0, 0, 0]
+        'apple', 'party', 5) == [1, 1, 0, 0, 0]
 
 
 def test_check_letter_duplicate():
     assert guess.check_letter(
-        'eaten', 'lever') == [0, 1, 0, 2, 0]
+        'eaten', 'lever', 5) == [0, 1, 0, 2, 0]
     assert guess.check_letter(
-        'apple', 'puple') == [1, 0, 2, 2, 2]
+        'apple', 'puple', 5) == [1, 0, 2, 2, 2]
     assert guess.check_letter(
-        'apple', 'puppy') == [1, 0, 2, 0, 0]
+        'apple', 'puppy', 5) == [1, 0, 2, 0, 0]
     assert guess.check_letter(
-        'tight', 'fight') == [0, 2, 2, 2, 2]
+        'tight', 'fight', 5) == [0, 2, 2, 2, 2]
 
 
 def test_highlight_letter():
@@ -78,14 +77,3 @@ def test_highlight_word():
     assert guess.highlight_word(
         'PUPLE', [1, 0, 2, 2, 2]
     ) == "[bold black on bright_yellow] P [/bold black on bright_yellow][bold black on white] U [/bold black on white][bold black on bright_green] P [/bold black on bright_green][bold black on bright_green] L [/bold black on bright_green][bold black on bright_green] E [/bold black on bright_green]\n"
-
-
-def test_export_record():
-    guess.export_record(['APPLE', 'PUPLE', 'PUPPY'], 'WATER', 'test')
-    with open('user_data/record_test.txt') as f:
-        data = f.readlines()
-        assert data == ['=================\n',
-                        '  | A P P L E |  \n',
-                        '  | P U P L E |  \n',
-                        '  | P U P P Y |  \n', '=================\n',
-                        'CORRECT WORD IS: WATER\n']
--- src/test_player.py
+++ src/test_player.py
@@ -1,31 +1,22 @@
 import pytest
-from player import Player, StartAgainException
+from player import Player
+from guess import StartAgainException
 
 
 @pytest.fixture
 def player():
-    return Player('pytest')
+    return Player()
 
 
 def test_get_spell_check_enabled(player):
     assert player.get_spell_check_enabled() == True
 
 
-def test_toggle_spell_check_enabled_confirm(player, monkeypatch, capsys):
-    monkeypatch.setattr('builtins.input', lambda _: 'Y')
-    with pytest.raises(StartAgainException):
-        player.toggle_spell_check_enabled()
+def test_toggle_spell_check_enabled_confirm(player, capsys):
+    player.toggle_spell_check_enabled()
     captured = capsys.readouterr()
     assert player.get_spell_check_enabled() == False
     assert captured.out == 'Spell check setting is now OFF.\n'
-
-
-def test_toggle_spell_check_enabled_cancel(player, monkeypatch, capsys):
-    monkeypatch.setattr('builtins.input', lambda _: 'N')
-    player.toggle_spell_check_enabled()
-    captured = capsys.readouterr()
-    assert player.get_spell_check_enabled() == True
-    assert captured.out == 'Back to the main game.\n'
 
 
 def test_get_records(player):
