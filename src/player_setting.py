8ee2d075fb844a65833a0b697a9e8ee84780814a
wenxuan-pan <panwenxuan42@gmail.com>
2023-05-13 15:49:23 +1000

    checked line limits in player setting module

--- src/player_setting.py
+++ src/player_setting.py
@@ -3,6 +3,7 @@ from rich import print
 
 
 def show_options():
+    # print a list of commands
     message = """           Commands (case-insensitive):
             [bold]1 - rename
             2 - toggle spell check
@@ -18,10 +19,12 @@ def show_options():
 
 
 def change_settings_loop(player):
+    # Repeatedly ask user to input commands until they
+    # choose to quit (break the while loop)
     while True:
         prompt = input(
-            "\n**What do you want to do? (Enter 'help' for a \
-list of commands)**\n").upper()
+            "\n**What do you want to do? (Enter 'help' for a"
+            "list of commands)**\n").upper()
         match prompt:
             case "1":
                 player.rename()
@@ -40,26 +43,36 @@ list of commands)**\n").upper()
             case "SHOW":
                 player.show_status()
             case "QUIT":
+                # ask if the changes are to be saved
                 will_save = input(
                     "\n**Save changes before quit?\n"
-                    "Type 'Y' to save, type any other buttons to discard all changes.**\n")
+                    "Type 'Y' to save, type any other buttons "
+                    "to discard all changes.**\n")
                 if will_save.upper() == "Y":
                     player.save_data()
                     print("**Changes saved.**\n")
                 else:
                     print("Changes discarded.")
+                # end the loop
                 break
+            # consider any other input to be invalid
             case other:
                 print("**Invalid input. Try again.**")
 
 
 def setting():
+    # create a player object
     player = create_player()
+    # get and display player name
     name = player.get_name()
     print(f"\n        Welcome, {name}!")
+    # show player profile
     player.show_status()
+    # show a list of commands
     show_options()
+    # ask for user input
     change_settings_loop(player)
+    # when loop ends, display quit message
     print(f"See you next time, {name}!")
 
 
